456efffbf76262dbe4be7cd48a07fbe74c56491d
Victor Khomenko <victor.khomenko@utdweb.team>
2022-05-28 01:02:30 +0300

    Create base pages, change file structure. Change ts.config, add prettier & js-cookie. Add default auth context.

---
+++ app/components/screens/auth/Auth.tsx
@@ -0,0 +1,10 @@
+import { useAuth } from '@/hooks/useAuth'
+import { FC } from 'react'
+
+const Auth: FC = () => {
+	const {} = useAuth()
+
+	return <div />
+}
+
+export default Auth
---
+++ app/services/auth/auth.helper.ts
@@ -0,0 +1,26 @@
+import Cookies from 'js-cookie'
+
+export type TypeUser = {
+	user: {
+		_id: string
+		email: string
+	} | null
+}
+
+export interface IAuthData {
+	user: TypeUser
+	accessToken: string
+}
+
+export const saveTokenToStorage = (accessToken: string) => {
+	Cookies.set('accessToken', accessToken)
+}
+
+export const removeTokenFromStorage = () => {
+	Cookies.remove('accessToken')
+}
+
+export const saveToStorage = (data: IAuthData) => {
+	saveTokenToStorage(data.accessToken)
+	localStorage.setItem('user', JSON.stringify(data.user))
+}
